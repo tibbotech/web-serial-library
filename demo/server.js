1e27ca0132a7ca87675e8ed27a9aee18eb47e643
Hsiang Chou <hsiang@tibbo.com>
2020-04-28 17:34:30 +0800

    Restructured folders. Compiles through NPM

---
+++ demo/server.js
@@ -0,0 +1,22 @@
+var fs = require('fs'),
+    path = require('path'),
+    http = require('http');
+
+const SERVER_PORT = 8899;
+
+http.createServer(function (req, res) {
+    let filename = path.join(__dirname, req.url);
+    if (req.url != '/index.html') {
+        filename = path.join(__dirname, '../build', req.url)
+    }
+    fs.readFile(filename, function (err, data) {
+        if (err) {
+            res.writeHead(404);
+            res.end(JSON.stringify(err));
+            return;
+        }
+        res.writeHead(200);
+        res.end(data);
+    });
+}).listen(SERVER_PORT);
+console.log('go to http://localhost:' + SERVER_PORT + '/index.html');
